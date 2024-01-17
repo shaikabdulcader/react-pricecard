4155acc29421825673440f221126861f8b0cd655
Shaik Abdul Cader <119555898+shaikabdulcader@users.noreply.github.com>
2024-01-17 06:49:47 +0530

    Update ProductCard3.jsx

--- src/ProductCard3.jsx
+++ src/ProductCard3.jsx
@@ -3,8 +3,8 @@ function ProductCard3(props){
         <div>
             <div className="card" style={{width: "18rem"}}>
             <div className="header">
-            <div className="card-head opacity-50"><h6>PRO</h6></div>
-            <h1 className="card-title">$49/month</h1>
+            <div className="card-head opacity-50"><h6>{props.plan}</h6></div>
+            <h1 className="card-title">{props.month}</h1>
             </div>
             <div className="card-body">
             <p className="card-text"><i class="fa fa-check" aria-hidden="true"></i> Single User</p>
